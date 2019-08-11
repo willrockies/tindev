4c673c8473614a55f64a6ab9f7917c34fdc74ab4
W. Alves da Silva <w.alves.da.silva@avanade.com>
2019-08-11 02:09:14 -0300

    somente commitando frontend

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Main.css";
+
+import api from "../services/api";
+import logo from "../assets/logo.svg";
+import dislike from "../assets/dislike.svg";
+import like from "../assets/like.svg";
+
+export default function({ match }) {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id,
+        },
+      });
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    //await api.post(`devs/${id}/dislikes`);
+  }
+  async function handleDislike(id) {
+    await api.post(`devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id },
+    });
+    setUsers(users.filter(user => user._id !== id));
+  }
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.avatar} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button">
+                  <img
+                    src={dislike}
+                    alt="dislike"
+                    onClick={() => handleDislike(user._id)}
+                  />
+                </button>
+                <button type="button">
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty"> acabou</div>
+      )}
+    </div>
+  );
+}
